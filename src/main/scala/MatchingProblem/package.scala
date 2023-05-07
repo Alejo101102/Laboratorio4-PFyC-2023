e29e67b8f088e55f344b39a6072a8fff75717e12
Alejandro Guerrero Cano <alejogc2002@hotmail.com>
2023-05-06 23:58:37 -0500

    Se hacen funciones matchByElement y matchsByElements

--- src/main/scala/MatchingProblem/package.scala
+++ src/main/scala/MatchingProblem/package.scala
@@ -4,27 +4,32 @@ package object MatchingProblem {
   type Preferences = Vector[Vector[Int]]
 
   def matchByElement(i: Int, n: Int): List[Match] = {
-
+   val posibles = for{
+     j<- 1 to n
+   } yield (i,j)
+    posibles.toList
   }
 
   def matchsByElements(n: Int): List[List[Match]] = {
-    //devuelvelalistadelosposibesmatchsdecadaelemento(1hastan) ...
+    val posibles = for {
+      j <- 1 to n
+    } yield matchByElement(j, n)
+    posibles.toList
   }
+  /*
+    def possibleMatchings(n: Int): List[List[Match]] = {
+    }
 
-  def possibleMatchings(n: Int): List[List[Match]] = {
-    //Devuelvelalistadetodoslosposiblesmatchingsdecadaunodelosn //elementos. ...
-  }
+      def matchings(n: Int): List[Matching] = {
+        //Devuelvelalistadetodoslosposiblesmatchingsdenpilotos ...
+      }
 
-  def matchings(n: Int): List[Matching] = {
-    //Devuelvelalistadetodoslosposiblesmatchingsdenpilotos ...
-  }
+      def weightedMatchings(n: Int, pilotPrefs: Preferences, navigPrefs: Preferences): List[(Matching, Int)] = {
+      }
 
-  def weightedMatchings(n: Int, pilotPrefs: Preferences, navigPrefs: Preferences): List[(Matching, Int)] = {
-  }
+      def bestMatching(n: Int, pilotPrefs: Preferences, navigPrefs: Preferences): (Matching, Int) = {
 
-  def bestMatching(n: Int, pilotPrefs: Preferences, navigPrefs: Preferences): (Matching, Int) = {
-
-  }
-}
+      }
+    */
 
 }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -2,9 +2,11 @@ import MatchingProblem._
 
 val pilot=Vector(Vector(2,3,1,1),Vector(1,1,4,3),Vector(1,2,3,4),Vector(2,3,2,1))
 val navig=Vector(Vector(4,1,3,2),Vector(4,2,3,1),Vector(1,1,1,4),Vector(3,2,3,3))
+
 matchByElement(2,5)
 matchByElement(3,5)
 matchsByElements(5)
+/*
 possibleMatchings(2)
 possibleMatchings(3)
 possibleMatchings(5)
@@ -18,3 +20,5 @@ weightedMatchings(4,pilot,navig)
 bestMatching(2,pilot,navig)
 bestMatching(3,pilot,navig)
 bestMatching(4,pilot,navig)
+
+ */
